2a46dcc3dfb29fb1a8f3d99f2fef2fb3dacfaabe
Vitalii A <crazyyy@gmail.com>
2017-12-20 01:46:56 +0200

    init

---
+++ html/js/slow_scroll.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$('a.slow').bind("click",function(e){var anchor=$(this);$('html, body').stop().animate({scrollTop:$(anchor.attr('href')).offset().top},1000);e.preventDefault();});});

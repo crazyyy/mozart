b25d6efc0386ca5042b4a25d58d30314b3dafe82
Vitalii A <crazyyy@gmail.com>
2017-12-21 02:59:46 +0200

    page partners ready

--- wordpress/wp-content/themes/wp-mozart/page.php
+++ wordpress/wp-content/themes/wp-mozart/page.php
@@ -1,15 +1,17 @@
 <?php get_header(); ?>
 
-  <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-      <h1 class="page-title inner-title"><?php the_title(); ?></h1>
-      <?php the_content(); ?>
-      <?php edit_post_link(); ?>
-
-    </article>
-  <?php endwhile; endif; ?>
-
-  <?php get_sidebar(); ?>
+    <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+      <?php if ( has_post_thumbnail()) { ?>
+        <div class="image-block"><img src="<?php echo the_post_thumbnail_url('full'); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>"  class="img-responsive center-block"></div>
+      <?php }  ?>
+      <!-- content -->
+      <div id="post-<?php the_ID(); ?>" <?php post_class('content'); ?>>
+        <div class="container text-big">
+          <div class="title"><?php the_title(); ?></div>
+          <?php the_content(); ?>
+        </div>
+        <div class="line line-margin"></div>
+      </div>
+    <?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
--- wordpress/wp-content/themes/wp-mozart/single.php
+++ wordpress/wp-content/themes/wp-mozart/single.php
@@ -1,34 +1,17 @@
 <?php get_header(); ?>
 
-  <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-      <h1 class="single-title inner-title"><?php the_title(); ?></h1>
-      <?php if ( has_post_thumbnail()) :?>
-        <a class="single-thumb" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-          <?php the_post_thumbnail(); // Fullsize image for the single post ?>
-        </a>
-      <?php endif; ?><!-- /post thumbnail -->
-
-      <span class="date"><?php the_time('d F Y'); ?> <?php the_time('H:i'); ?></span>
-      <span class="author"><?php _e( 'Published by', 'wpeasy' ); ?> <?php the_author_posts_link(); ?></span>
-      <span class="comments"><?php comments_popup_link( __( 'Leave your thoughts', 'wpeasy' ), __( '1 Comment', 'wpeasy' ), __( '% Comments', 'wpeasy' )); ?></span><!-- /post details -->
-
-      <?php the_content(); ?>
-
-      <?php the_tags( __( 'Tags: ', 'wpeasy' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
-
-      <p><?php _e( 'Categorised in: ', 'wpeasy' ); the_category(', '); // Separated by commas ?></p>
-
-      <p><?php _e( 'This post was written by ', 'wpeasy' ); the_author(); ?></p>
-
-      <?php edit_post_link(); ?>
-
-      <?php comments_template(); ?>
-
-    </article>
-  <?php endwhile; endif; ?>
-
-  <?php get_sidebar(); ?>
+    <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+      <?php if ( has_post_thumbnail()) { ?>
+        <div class="image-block"><img src="<?php echo the_post_thumbnail_url('full'); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>"  class="img-responsive center-block"></div>
+      <?php }  ?>
+      <!-- content -->
+      <div id="post-<?php the_ID(); ?>" <?php post_class('content'); ?>>
+        <div class="container text-big">
+          <div class="title"><?php the_title(); ?></div>
+          <?php the_content(); ?>
+        </div>
+        <div class="line line-margin"></div>
+      </div>
+    <?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
